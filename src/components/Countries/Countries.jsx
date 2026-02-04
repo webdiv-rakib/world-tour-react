48b990a80340b513ace928068d509bc3b000f161
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-05 03:50:03 +0600

    Display countries in a simple way, Folder Structure

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+
+const Countries = () => {
+    const [countries, setCountries] = useState([])
+    useEffect(() => {
+        fetch('https://openapi.programming-hero.com/api/all')
+            .then(res => res.json())
+            .then(data => setCountries(data.countries))
+            .catch(err => console.log(err))
+    }, [])
+    return (
+        <div>
+            <h1>Countries</h1>
+            <h4>Total Countries: {countries.length}</h4>
+            {
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,13 @@
+import './Country.css'
+const Country = ({ country }) => {
+    console.log(country);
+    const { name, flags } = country;
+    return (
+        <div className="country">
+            <h3>Name: {name?.common}</h3>
+            <img src={flags.flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
