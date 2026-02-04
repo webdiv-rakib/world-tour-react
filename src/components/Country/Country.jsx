6a3f0d1dfa93015155c969f87feb662585a52dd2
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-05 04:48:32 +0600

    added population and area.

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,12 @@
 import './Country.css'
 const Country = ({ country }) => {
     console.log(country);
-    const { name, flags } = country;
+    const { name, flags, population, area } = country;
     return (
         <div className="country">
             <h3>Name: {name?.common}</h3>
             <img src={flags.flags.png} alt="" />
+            <p>Population: {population.population}</p> <p>Area: {area.area}</p>
         </div>
     );
 };
